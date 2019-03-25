475711c471218b693c5ec3807db376aae49dd1f5
pwachira900@gmail.com <pwachira900@gmail.com>
2019-03-25 14:16:40 +0300

    added increment for upvote

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,6 +9,8 @@ import {Quote} from "../quote"
 
 export class DetailsComponent implements OnInit {
   @Input() quote:Quote
+  upvotes=0;
+  downvotes=0;
   constructor() { }
 
   ngOnInit() {
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,8 +3,8 @@ export class Quote {
   public downvotes:number
   public myDate:Date
   constructor(public name:string, public author:string,public quote:string){
-    this.upvotes=0;
-    this.downvotes=0;
-    this.myDate=new Date();
+    this.upvotes =0;
+    this.downvotes =0;
+    this.myDate=new Date;
   }
 }
