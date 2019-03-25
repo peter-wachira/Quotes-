fd3f3d2b64dc4cb315619dbcfe50fd943f936d60
pwachira900@gmail.com <pwachira900@gmail.com>
2019-03-25 23:54:07 +0300

    add date to the constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,10 @@
 export class Quote {
   public upvotes:number
   public downvotes:number
-  public myDate:Date
+  public completeDate:Date
   constructor(public name:string, public author:string,public quote:string){
     this.upvotes =0;
     this.downvotes =0;
-    this.myDate=new Date;
+    this.completeDate=new Date;
   }
 }
