1de1bc6117e7d9f6c0cd546a52442a4c4edd593e
pwachira900@gmail.com <pwachira900@gmail.com>
2019-03-26 10:26:29 +0300

    Deleted dist

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes = [
     new Quote("MLK", "Martin Luther King", "Injustice anywhere is a threat to justice everywhere."),
-    new Quote("Life", "Margeret Atwood", "In the end we all become stories   ")
+    new Quote("Life", "Margeret Atwood", "In the end we all become stories ")
   ]
+    
 
   addQuote(chosenQuote) {
     this.quotes.push(chosenQuote)
